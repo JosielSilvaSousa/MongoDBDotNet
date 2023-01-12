04fca78bc1ee89a205a344d51f5cbc1631888794
unknown <josielsilva84@gmail.com>
2023-01-12 17:05:08 -0300

    Primeiro Commit

---
+++ CursoMongo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CursoMongo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainSync(args);
+            Console.WriteLine("Pressione Enter");
+            Console.ReadLine();
+        }
+
+        static void MainSync(string[] args)
+        {
+            Console.WriteLine("Esperando 10 segundos.....");
+            System.Threading.Thread.Sleep(10000);
+            Console.WriteLine("Esperando 10 segubndos.....");
+        }
+    }
+}
---
+++ CursoMongo/ProgramAssicono.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoMongo
+{
+    class ProgramAssicono
+    {
+
+            static void Main(string[] args)
+            {
+                Task T = MainASync(args);
+                Console.WriteLine("Pressione Enter");
+                Console.ReadLine();
+            }
+
+            static async Task MainASync(string[] args)
+            {
+                Console.WriteLine("Esperando 10 segundos.....");
+                await Task.Delay(10000);
+                Console.WriteLine("Esperando 10 segubndos.....");
+            }
+
+    }
+}
