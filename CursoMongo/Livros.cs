f1a2c61dfe6806f489d781b1e11b990dbec0db4c
unknown <josielsilva84@gmail.com>
2023-01-13 16:42:14 -0300

    Manipulando classe

---
+++ CursoMongo/Livros.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CursoMongo
+{
+    public class Livros
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int Ano { get; set; }
+        public int pagina { get; set; }
+        public List<string> Assunto { get; set; }
+    }
+}
---
+++ CursoMongo/manipulandoClasses.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace CursoMongo
+{
+    class manipulandoClasses
+    {
+        static void Main(string[] args)
+        {
+            Task T = MainASync(args);
+            Console.WriteLine("Pressione Enter");
+            Console.ReadLine();
+        }
+
+        static async Task MainASync(string[] args)
+        {
+            // var doc = new BsonDocument {
+            //     { "Titulo","Guerra dos Tronos" }
+            //     };
+            // doc.Add("autor", "George R R Martin");
+            // doc.Add("Ano", 1990);
+            //  doc.Add("Paginas", 856);
+
+            // var assuntoArray = new BsonArray();
+            // assuntoArray.Add("Fantasil");
+            //  assuntoArray.Add("Ação");
+
+            //  doc.Add("Assunto", assuntoArray);
+
+            // Console.WriteLine(doc);
+
+            Livros livro = new Livros();
+            livro.Titulo = "Sob a redoma";
+            livro.Autor = "Stepahn King";
+            livro.Ano = 2022;
+            livro.pagina = 679;
+
+            List<string> listaAssunto = new List<string>();
+            listaAssunto.Add("Ficção Cientifica");
+            listaAssunto.Add("Terros");
+            listaAssunto.Add("Ação");
+            livro.Assunto = listaAssunto;
+
+
+            //Acesso ao servidor ao mongo DB
+            string stringConexao = "mongodb://localhost:27017";
+            IMongoClient cliente = new MongoClient(stringConexao);
+
+            //acessando ao banco de dados.
+
+            IMongoDatabase bancoDados = cliente.GetDatabase("Biblioteca");
+
+            //acessando a colletion
+            IMongoCollection<Livros> colecao = bancoDados.GetCollection<Livros>("Livros");
+
+            //Incluindo documento
+
+            await colecao.InsertOneAsync(livro);
+
+            Console.WriteLine("Documento incluido com sucesso");
+
+        }
+    }
+}
