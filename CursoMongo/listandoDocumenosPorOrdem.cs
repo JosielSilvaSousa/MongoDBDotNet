f783c1e19efbe77d42143361ce991105e6b6853a
unknown <josielsilva84@gmail.com>
2023-01-17 10:04:56 -0300

    Filtros com ordenação e limete de paginas

---
+++ CursoMongo/listandoDocumenosPorOrdem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace CursoMongo
+{
+    class listandoDocumenosPorOrdem
+    {
+        static void Main(string[] args)
+        {
+            Task T = MainASync(args);
+            Console.WriteLine("Pressione Enter");
+            Console.ReadLine();
+        }
+
+        static async Task MainASync(string[] args)
+        {
+
+
+            var conexaoBiblioteca = new ConectandoMongoDB();
+
+
+
+           Console.WriteLine("Listando Documentos mais de 100 paginas");
+
+            var contructor = Builders<Livros>.Filter;
+            var condicao = contructor.Gt(x => x.Pagina, 100);
+
+
+           var  listaLivros = await conexaoBiblioteca.Livros.Find(condicao).SortBy(x => x.Titulo).Limit(5).ToListAsync();
+
+            foreach (var doc in listaLivros)
+            {
+                Console.WriteLine(doc.ToJson<Livros>());
+            }
+
+            Console.WriteLine("Fim da lista");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+          
+
+
+          
+
+        }
+    }
+}
