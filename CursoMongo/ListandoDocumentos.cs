8d9702b887f6a77e33e7a00db62f0a7e82fbf3a0
unknown <josielsilva84@gmail.com>
2023-01-17 08:56:21 -0300

    Listando documentos

---
+++ CursoMongo/ListandoDocumentos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace CursoMongo
+{
+    class ListandoDocumentos
+    {
+        static void Main(string[] args)
+        {
+            Task T = MainASync(args);
+            Console.WriteLine("Pressione Enter");
+            Console.ReadLine();
+        }
+
+        static async Task MainASync(string[] args)
+        {
+
+
+            var conexaoBiblioteca = new ConectandoMongoDB();
+
+
+           Console.WriteLine("Listando Documentos");
+                                                                 //New BSONDOCUMENTO para informar que não tem criterio para busca.
+            var listaLivros = await conexaoBiblioteca.Livros.Find(new BsonDocument()).ToListAsync();
+
+            foreach(var doc in listaLivros)
+            {
+                Console.WriteLine(doc.ToJson<Livros>());
+            }
+
+            Console.WriteLine("Fim da lista");
+        }
+    }
+}
---
+++ CursoMongo/listandoDocumentosFindBson.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace CursoMongo
+{
+    class listandoDocumentosFindBson
+    {
+
+        static void Main(string[] args)
+        {
+            Task T = MainASync(args);
+            Console.WriteLine("Pressione Enter");
+            Console.ReadLine();
+        }
+
+        static async Task MainASync(string[] args)
+        {
+
+
+            var conexaoBiblioteca = new ConectandoMongoDB();
+
+
+            Console.WriteLine("Listando Documentos");
+
+            var filtro = new BsonDocument();
+            var listaLivros = await conexaoBiblioteca.Livros.Find(filtro).ToListAsync();
+
+            foreach (var doc in listaLivros)
+            {
+                Console.WriteLine(doc.ToJson<Livros>());
+            }
+
+            Console.WriteLine("Fim da lista");
+
+
+            Console.WriteLine("Listando Documentos Autor = Machado de Assis");
+
+
+            filtro = new BsonDocument()
+             {
+                 {"Autor","Machado de Assis" }
+             };
+            
+             listaLivros = await conexaoBiblioteca.Livros.Find(filtro).ToListAsync();
+
+            foreach (var doc in listaLivros)
+            {
+                Console.WriteLine(doc.ToJson<Livros>());
+            }
+
+            Console.WriteLine("Fim da lista");
+        }
+    }
+}
